730b7e75637e1b1c4e7095bb77e8a1e13a820d3e
Amlanjyoti Rava <ravaamlan108@gmail.com>
2025-07-05 16:55:16 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -11,14 +11,25 @@ import fileUpload from "express-fileupload";
 
 const app = express();
 config({ path: "./config/config.env" });
+const allowedOrigins = [
+  process.env.FRONTEND_URL,           // production frontend
+  "http://localhost:5173"             // local frontend for development
+];
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
-    method: ["GET", "POST", "DELETE", "PUT"],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
 );
+
 
 app.use(cookieParser());
 app.use(express.json());
@@ -30,10 +41,17 @@ app.use(
     tempFileDir: "/tmp/",
   })
 );
+
+ 
+
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/job", jobRouter);
 app.use("/api/v1/application", applicationRouter);
 dbConnection();
+app.get('/', (req, res) => {
+  res.send('app is working');
+});
 
+ 
 app.use(errorMiddleware);
 export default app;
