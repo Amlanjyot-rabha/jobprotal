1aa61ad1b99f2fffb6fb5105baa821f7b2e5286b
Amlanjyoti Rava <ravaamlan108@gmail.com>
2025-07-05 16:23:39 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,9 @@ cloudinary.v2.config({
   api_key: process.env.CLOUDINARY_API_KEY,       // CLOUDINARY_CLIENT_API
   api_secret: process.env.CLOUDINARY_API_SECRET, // CLOUDINARY_CLIENT_SECRET
 });
-
+app.get('/',(req,res)=>{
+    res.send('app is working')
+})
 app.listen(process.env.PORT, () => {
   console.log(`Server running at port ${process.env.PORT}`);
 });
