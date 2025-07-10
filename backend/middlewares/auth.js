e0d93134fe42f883d975b96371aa43a32b997230
Amlanjyoti.rava <132035645+Amlanjyot-rabha@users.noreply.github.com>
2025-07-10 14:12:38 +0530

    Update auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -5,11 +5,17 @@ import jwt from "jsonwebtoken";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   const { token } = req.cookies;
+  
   if (!token) {
-    return next(new ErrorHandler("User Not Authorized", 401));
+    return next(new ErrorHandler("User not authenticated", 401));
   }
-  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-  req.user = await User.findById(decoded.id);
-  next();
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.user = await User.findById(decoded.id);
+    next();
+  } catch (error) {
+    return next(new ErrorHandler("Invalid or expired token", 401));
+  }
 });
  
