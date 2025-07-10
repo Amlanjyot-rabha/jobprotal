a5c055f7c4da633be710c1eb8e45a2db66b337cb
Amlanjyoti.rava <132035645+Amlanjyot-rabha@users.noreply.github.com>
2025-07-10 16:01:01 +0530

    Update Login.jsx

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -43,7 +43,7 @@ const Login = () => {
 
   //  const value = localStorage.getItem('isLoggedIn') === 'true';
    
-  if(locatStorage.getItem('auth')==='true'){
+  if(localStorage.getItem('auth')==='true'){
     return <Navigate to={'/'}/>
   }
 
